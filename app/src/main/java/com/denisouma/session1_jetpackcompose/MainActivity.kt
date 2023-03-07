8f7d3f5b32ff242381d8e7c6a82eb8a23e66eb7c
Ouma254 <oumadeni33@gmail.com>
2023-03-07 18:34:29 +0300

    project finished

--- app/src/main/java/com/denisouma/session1_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/denisouma/session1_jetpackcompose/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -31,82 +34,71 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Session1JetPackComposeTheme {
-               //MyName()
-               BootCampLearners()
-                //ImagesExample()
-                //FlowerName()
+                Scaffold(
+
+
+                    topBar = {
+
+                      TopAppBar(
+
+                          backgroundColor = Color.Gray
+                      ) {
+                          Row(
+                              modifier = Modifier.fillMaxWidth(),
+                              horizontalArrangement = Arrangement.SpaceBetween,
+                              verticalAlignment = Alignment.CenterVertically
+                          ) {
+                              Image(
+                                  modifier = Modifier.size(25.dp),
+                                  painter = painterResource(id = R.drawable.man ), contentDescription = null)
+                              Icon(
+                                  modifier = Modifier.size(30.dp),
+                                  imageVector = Icons.Default.Notifications,
+                                  contentDescription =null, )
+                          }
+
+
+                      }
+                  }
+                ) {
+                    LazyRow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                         ){
+                        items(50){
+
+                            Card(
+                                modifier = Modifier
+                                    .padding(10.dp)) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.leaf),
+                                    contentDescription =null )
+                                Box(modifier = Modifier
+                                    .padding(
+                                        horizontal = 0.dp,
+                                        vertical = 25.dp
+                                    )
+                                    .background(color = Color.Transparent)) {
+                                    Text(text = "Mount Kenya",
+                                        modifier = Modifier.align(Alignment.BottomCenter),
+                                        //fontWeight = 10.sp,
+                                        //fontFamily = "Sans serif",
+                                        )
+                                    Text(text = "Meru, Kenya")
+
+                                }
+
+                            }
+                        }
+
+                    }
+
+
+                }
             }
         }
     }
 }
 
-@Composable
-fun MyName(){
-    Text(text = "ANDROID BOOTCAMP",
-         color = Color.Blue,
-         fontSize = 25.sp,
-         fontWeight = FontWeight.Light,
-         modifier = Modifier
-             .padding(start = 16.dp, top = 4.dp, end = 0.dp, bottom = 2.dp)
-             .fillMaxSize()
-             .fillMaxHeight()
-             .background(Color.Green)
-    
-    )
-}
-
-@Composable
-fun BootCampLearners() {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .background(Color.Blue)
-
-    ) {
-        MyName()
-        Text(text = "Kevin")
-        Text(text = "clinton")
-        Text(text = "Denis")
-        
-    }
-    
-}
-
-@Composable
-fun FlowerName() {
-    Row (
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            ){
-        Text(text = "SUNFLOWER",
-             fontSize = 22.sp,
-             color = Color.Yellow
-        )
-        Text(text = "$500")
-    }
-    
-}
-
-@Composable
-fun BoxExample() {
-    Box (modifier = Modifier
-        .padding(16.dp)
-        .width(200.dp)
-        .height(200.dp)
-        .background(Color.Red)) {
-        Text(text = "HELLO WORLD",
-             modifier = Modifier.align(Alignment.BottomCenter))
-        Text(text = "world of fantancy",
-             modifier = Modifier.align(Alignment.BottomCenter)
-             )
-    }
-    
-}
-
-//@Composable
-//fun ImagesExample() {
